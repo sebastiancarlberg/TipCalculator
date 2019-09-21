0b4eb2335167eaf546fc3c7b45b49cd2ffcb5925
Sebastian Carlberg <carlberg.sebastian@gmail.com>
2019-09-21 17:36:54 +0200

    Initial Commit

---
+++ TipCalculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TipCalculator
+//
+//  Created by Sebastian Carlberg on 2019-09-21.
+//  Copyright © 2019 Sebastian Carlberg. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
